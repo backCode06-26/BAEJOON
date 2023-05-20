21df229cf1ab8b7cbee039218ecf0cd799ce8d10
Yoohojin <118101197+application1324@users.noreply.github.com>
2023-05-20 21:05:42 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 272 ms, Memory: 15644 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+
+		for(int i=0; i<T; i++) {
+			for(int j=0; j<T-i-1; j++) {
+				System.out.print(" ");	
+			}
+			for(int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
