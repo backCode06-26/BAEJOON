9420af3f0f690dbfd67bb067c356c6578cc58121
Yoohojin <118101197+application1324@users.noreply.github.com>
2023-07-23 22:08:14 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 12800 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -4,13 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        int cnt = 0;
         String[] strs = new String[5];
         for(int i=0; i<5; i++) {
         	strs[i] = sc.nextLine();
+        	cnt += strs[i].length();
+        	
         }
         
         int i = 0;
-        while (i <= 90) {
+        while (i <= cnt) {
             	for(int j=0; j<5; j++) {
             		if(i >= 0 && i < strs[j].length()) {
             			System.out.print(strs[j].charAt(i));
