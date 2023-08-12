82c1f8e560be5a3f9cd2902a6671703001897c7f
Yoohojin <118101197+application1324@users.noreply.github.com>
2023-08-12 21:08:00 +0900

    [Bronze IV] Title: 세수정렬, Time: 120 ms, Memory: 12928 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int[] nums = new int[3];
+    for(int i=0; i<nums.length; i++) {
+    	nums[i] = sc.nextInt();
+    }
+    
+    Arrays.sort(nums);
+    
+    for(int num : nums) {
+    	System.out.print(num + " ");
+    }
+    
+    sc.close();
+    }
+}
+
