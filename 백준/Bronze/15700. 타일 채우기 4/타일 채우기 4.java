0ba91dbf357a07f9eaa81cbd6709a7ca4f817d56
Yoohojin <118101197+application1324@users.noreply.github.com>
2023-08-19 20:57:12 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 112 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long result = (sc.nextLong()*sc.nextLong())/2;
+        System.out.println(result);
+    }
+}
+
