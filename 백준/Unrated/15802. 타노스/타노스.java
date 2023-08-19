c0176d62326b15b1062b0bbb0ac893977b07eb1e
Yoohojin <118101197+application1324@users.noreply.github.com>
2023-08-19 21:32:25 +0900

    [Unrated] Title: 타노스, Time: 80 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Unrated/15802. 타노스/타노스.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String name = br.readLine();
+        
+        if(name.equals("chocolate")){
+            bw.write(String.valueOf("1"));
+        }else{
+            bw.write(String.valueOf("0"));
+        }
+        
+        bw.flush();
+    }
+ 
+}
