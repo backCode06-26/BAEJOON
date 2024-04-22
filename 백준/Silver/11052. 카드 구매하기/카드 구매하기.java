e71b0ea3c0978e128edeed777db08a454644c9de
Yoohojin <118101197+pullCoding@users.noreply.github.com>
2024-04-23 08:28:21 +0900

    [Silver I] Title: 카드 구매하기, Time: 196 ms, Memory: 17416 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] prices = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + prices[j]);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
