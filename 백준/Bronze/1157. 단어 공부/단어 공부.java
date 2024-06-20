7b04818697c6913d9fa1cbcc49bb02793acf7601
Yoohojin <118101197+pullCoding@users.noreply.github.com>
2024-06-20 17:15:53 +0900

    [Bronze I] Title: 단어 공부, Time: 308 ms, Memory: 30376 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,32 +1,36 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String word = sc.next().toUpperCase(); // 입력된 단어를 대문자로 변환하여 저장
+        // 서로 다른 알파벳의 개수을 출력
+        Scanner in = new Scanner(System.in);
 
-        int[] count = new int[26]; // 알파벳 개수만큼의 배열을 생성하여 각 알파벳의 빈도를 저장
-        for (int i = 0; i < word.length(); i++) {
-            char letter = word.charAt(i);
-            if (Character.isLetter(letter)) { // Character.isLetter()는 알파벳인지 판별하기
-                int index = letter - 'A'; // 알파벳의 인덱스을 구함
-                count[index]++;
+        int max = 0;
+        char ch = ' ';
+
+        int[] nums = StringCount(in.next().toLowerCase());
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] > max) {
+                max = nums[i];
+                ch = (char) (65+i);
+            } else if(nums[i] == max) {
+                ch = '?';
             }
         }
+        System.out.println(ch);
+    }
 
-        int maxCount = 0;
-        char mostFrequentLetter = '?';
-        for (int i = 0; i < count.length; i++) {
-            if (count[i] > maxCount) {
-                maxCount = count[i];
-                mostFrequentLetter = (char) (i + 'A');
-//                이것은 count[i]가 maxCount보다 클때 i을 'A'에 더하면 i에 해당하는 알파벳이 mostFrequentLetter에 담겨짐
-            } else if (count[i] == maxCount) {
-                mostFrequentLetter = '?';
-            }
+    public static int[] StringCount(String str) {
+        int[] count = new int[26];
+
+        char[] arr = str.toCharArray();
+        for(int i=0; i<arr.length; i++) {
+            count[arr[i]-'a']++;
         }
 
-        System.out.println(mostFrequentLetter);
-        sc.close();
+        return count;
     }
 }
