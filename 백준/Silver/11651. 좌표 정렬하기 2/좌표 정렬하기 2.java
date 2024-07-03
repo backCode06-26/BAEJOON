5ffac1ed09e287a4b062f0d2a2c284ac278e9563
Yoohojin <118101197+backCode06-26@users.noreply.github.com>
2024-07-04 01:21:31 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2088 ms, Memory: 193032 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < N; i++) {
+            arr[i][0] = in.nextInt();
+            arr[i][1] = in.nextInt();
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[1] == e2[1]) {
+                // 값이 같다면 각 요소의 뒤 숫자를 오름차순으로 정렬한다.
+                return e1[0] - e2[0];
+            } else {
+                // 값이 같다면 각 요소의 앞 숫자를 오름차순으로 정렬한다.
+                return e1[1] - e2[1];
+            }
+        });
+
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
