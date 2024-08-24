c0640e91aa23c82e4d3bd061d29c91edb2980a77
Yoohojin <118101197+backCode06-26@users.noreply.github.com>
2024-08-24 20:59:01 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 16260 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,32 +1,22 @@
-
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-	  Scanner sc = new Scanner(System.in);
-	  
-	  int n = sc.nextInt();
-	  
-	  int max = 0;
-	  double average = 0;
-	  int[] num = new int[n];
-	  
-	  for(int i=0; i<n; i++) {
-		  num[i] = sc.nextInt();
-		 
-		  if(num[i] > max) {
-			  max = num[i];
-		  }
-		  
-	  }
-	  
-	  for(int i=0; i<n; i++) {
-		  average += (double)num[i] / max * 100;
-//		  double값을 나타날때는 나누거나 곱하는 값에 double타입으로 형변환 한다.
-	  }
-	  
-	 System.out.println(average/n);
-	  
-  }
-  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        long sum = 0;
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, a[i]);
+            sum += a[i];
+        }
+
+        System.out.println(sum * 100.0 / max / n);
+    }
 }
