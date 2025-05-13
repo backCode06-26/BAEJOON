fea89f63827fc55db14714b91d4d0e576ffc2d8a
Yoohojin <118101197+backCode06-26@users.noreply.github.com>
2025-05-13 13:32:58 +0900

    [Bronze III] Title: 학점계산, Time: 88 ms, Memory: 12760 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String grade = sc.nextLine();
+
+        String result = "";
+        switch (grade) {
+            case "A+":
+                result = "4.3";
+                break;
+            case "A0":
+                result = "4.0";
+                break;
+            case "A-":
+                result = "3.7";
+                break;
+            case "B+":
+                result = "3.3";
+                break;
+            case "B0":
+                result = "3.0";
+                break;
+            case "B-":
+                result = "2.7";
+                break;
+            case "C+":
+                result = "2.3";
+                break;
+            case "C0":
+                result = "2.0";
+                break;
+            case "C-":
+                result = "1.7";
+                break;
+            case "D+":
+                result = "1.3";
+                break;
+            case "D0":
+                result = "1.0";
+                break;
+            case "D-":
+                result = "0.7";
+                break;
+            case "F":
+                result = "0.0";
+                break;
+        }
+        System.out.println(result);
+    }
+}
