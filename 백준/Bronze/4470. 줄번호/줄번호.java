5cec3d11c62037962da9ec39321d7ce8a7061737
Yoohojin <118101197+backCode06-26@users.noreply.github.com>
2025-07-28 19:02:05 +0900

    [Bronze IV] Title: 줄번호, Time: 84 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 1; i <= n; i++) {
+            String s = sc.nextLine();
+            sb.append(i).append(". ").append(s).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
